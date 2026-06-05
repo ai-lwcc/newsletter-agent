3c0452afee8411723b3140d973fb6b46e01513fa
lwccai <lwccai@lwcounselling.ca>
2026-06-05 10:35:35 -0400

    add campaign preview links to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import Campaign, DeliveryLog, Group, Person
+from django.urls import reverse
+from django.utils.html import format_html
+
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
@@ -29,6 +31,7 @@ class CampaignAdmin(admin.ModelAdmin):
         "title",
         "email_subject",
         "status",
+        "preview_link",
         "scheduled_send_time",
         "dry_run_completed_at",
         "pdf_attachment",
@@ -38,6 +41,23 @@ class CampaignAdmin(admin.ModelAdmin):
     list_filter = ("status", "target_groups", "scheduled_send_time")
     search_fields = ("title", "email_subject", "email_body", "whatsapp_message")
     filter_horizontal = ("target_groups",)
+
+    def preview_link(self, obj):
+        url = reverse("campaign_preview", kwargs={"campaign_id": obj.id})
+        return format_html('<a href="{}">Preview</a>', url)
+
+    preview_link.short_description = "Preview"
+
+    readonly_fields = ("preview_page",)
+
+    def preview_page(self, obj):
+        if not obj.id:
+            return "Save this campaign first to preview it."
+
+        url = reverse("campaign_preview", kwargs={"campaign_id": obj.id})
+        return format_html('<a href="{}">Open preview page</a>', url)
+
+    preview_page.short_description = "Preview Page"
 
 
 @admin.register(DeliveryLog)
