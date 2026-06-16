091468003a4e3d1f577e9accce10778d500008c8
lwccai <lwccai@lwcounselling.ca>
2026-06-16 12:47:24 -0400

    add manager-only access controls

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 @pytest.fixture
@@ -13,6 +13,31 @@ def authenticated_client(client):
     client.login(
         username="testuser",
         password="testpassword123",
+    )
+
+    return client
+
+@pytest.fixture
+def manager_user(db):
+    group, _ = Group.objects.get_or_create(
+        name="Newsletter Manager",
+    )
+
+    user = User.objects.create_user(
+        username="manager",
+        password="password",
+    )
+
+    user.groups.add(group)
+
+    return user
+
+
+@pytest.fixture
+def manager_client(client, manager_user):
+    client.login(
+        username="manager",
+        password="password",
     )
 
     return client
